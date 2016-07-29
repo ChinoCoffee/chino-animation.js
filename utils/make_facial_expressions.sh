3a5b98d67ffa631b1398baba1c03c21764a88b40
unikko <chino.coffee.1204@gmail.com>
2016-07-30 05:03:19 +0900

    Subtle change. Add EyeController

--- utils/make_facial_expressions.sh
+++ utils/make_facial_expressions.sh
@@ -1,2 +1,1 @@
-python svg_diff.py ../references/chino-bezier-peace.svg ../references/chino-bezier-angry.svg ../references/chino-bezier-eyes-shut.svg ../references/chino-bezier-sad.svg > ../references/facial-expressions.json 
-
+python svg_diff.py ../references/chino-bezier-peace.svg ../references/chino-bezier-angry.svg ../references/chino-bezier-surprise.svg ../references/chino-bezier-eyes-shut.svg ../references/chino-bezier-sad.svg > ../references/facial-expressions.json
